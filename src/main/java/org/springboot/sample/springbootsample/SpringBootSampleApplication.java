3c96698f9d5a3a1df576084a9dbd5ef3775747fa
hyf <hyf_109@163.com>
2018-05-12 14:39:02 +0800

    添加注释

--- src/main/java/org/springboot/sample/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/org/springboot/sample/springbootsample/SpringBootSampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringBootSampleApplication {
-
+	//ceshi
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
